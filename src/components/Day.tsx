8dcc71f5d927a94ea1c5e98a3dad567dcd9a65dc
Vitor Mateus <vitor7jan@gmail.com>
2022-06-04 00:13:54 -0300

    day navigation

--- src/components/Day.tsx
+++ src/components/Day.tsx
@@ -1,5 +1,9 @@
-import { useSelector } from "react-redux";
-import { RootState } from "../store/store";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  decrementDay,
+  incrementDay,
+} from "../store/ducks/currentDate/currentDate.slice";
+import { AppDispatch, RootState } from "../store/store";
 
 export const Day = () => {
   const currentDate = useSelector(
@@ -16,9 +20,21 @@ export const Day = () => {
     return `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`;
   };
 
+  const dispatch = useDispatch<AppDispatch>();
+
+  const handleIncrementDay = () => {
+    dispatch(incrementDay());
+  };
+
+  const handleDecrementDay = () => {
+    dispatch(decrementDay());
+  };
+
   return (
     <div>
-      <p>hello month</p>
+      <p>hello day: {currentDate.getDate()}</p>
+      <button onClick={handleDecrementDay}>{`<<`}</button>
+      <button onClick={handleIncrementDay}>{`>>`}</button>
       <div>
         {day?.events.map((e) => {
           return (
