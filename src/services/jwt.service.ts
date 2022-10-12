4403a034c249ef1141e611f1b1c657e585852f27
Vitor Mateus <vitor7jan@gmail.com>
2022-10-12 11:11:01 -0300

    fix(service): fix jwt decode

--- src/services/jwt.service.ts
+++ src/services/jwt.service.ts
@@ -1,15 +1,17 @@
+import jwtDecodePackage from "jwt-decode";
+
 export type jwtData = {
   jwt: string;
 };
 
-const localStorageKey = "eventManagerKey";
+const localStorageKey = "eventManagerKeyaaa";
 
-export const saveJwt = (jwtData: jwtData): boolean => {
+export const saveJwt = (jwtData: jwtData): void => {
   localStorage.setItem(localStorageKey, jwtData.jwt);
 
   const savedJwt = findJwt();
 
-  return jwtData.jwt === savedJwt ? true : false;
+  if (!savedJwt || savedJwt !== jwtData.jwt) throw new Error("Error to save jwt in local storage.");
 };
 
 export const findJwt = (): string | null => {
@@ -18,10 +20,23 @@ export const findJwt = (): string | null => {
   return jwt ?? null;
 };
 
-export const removeJwt = (): boolean => {
+export const removeJwt = (): void => {
   localStorage.removeItem(localStorageKey);
 
   const jwt = findJwt();
 
-  return jwt ? false : true;
+  if(jwt) throw new Error("Error to remove jwt from local storage.");
+};
+
+export const decodeJwt = (jwt: string): number => {
+  const decodedJwt = jwtDecodePackage(jwt);
+
+  if (!decodedJwt) throw new Error("Jwt cannot be decoded.");
+
+  //@ts-ignore
+  const userId: number = +decodedJwt.userId;
+
+  if (!userId) throw new Error("User id not find in jwt.");
+  
+  return userId;
 };
