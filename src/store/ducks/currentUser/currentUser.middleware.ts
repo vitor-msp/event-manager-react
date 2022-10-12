7b57410f8e25f4da1b968e06d6e9beffbaa0f536
Vitor Mateus <vitor7jan@gmail.com>
2022-10-12 10:11:58 -0300

    feat(middleware): save jwt in login

--- src/store/ducks/currentUser/currentUser.middleware.ts
+++ src/store/ducks/currentUser/currentUser.middleware.ts
@@ -1,3 +1,4 @@
+import { saveJwt } from "../../../services/jwt.service";
 import { AppThunk } from "../../store";
 import { loginUser } from "./currentUser.slice";
 import { ILoginRequest } from "./currentUser.types";
@@ -8,8 +9,12 @@ export const loginRequest =
     try {
       // simulate request;
       const res = {
-        data: { id: 1 },
+        data: { id: 1, jwt: "jwt-test" },
       };
+
+      const jwtIsSaved = saveJwt({ jwt: res.data.jwt });
+
+      if (!jwtIsSaved) throw new Error("Jwt cannot be saved in local storage.");
 
       dispatch(loginUser(res.data));
     } catch (error) {
