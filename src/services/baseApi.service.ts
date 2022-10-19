bb64eeb28b3243959844fbea8c0915b6c3542a45
Vitor Mateus <vitor7jan@gmail.com>
2022-10-18 22:54:54 -0300

    chore(env): isolate api url in .env file

--- src/services/baseApi.service.ts
+++ src/services/baseApi.service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export const injectJwt = (jwt: string) => {
